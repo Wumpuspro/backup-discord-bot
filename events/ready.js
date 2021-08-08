21884c369a0f0ab945545782d67e57c4f21d9086
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-08 22:23:58 +0545

    mhm les go

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = (client) => {
+    console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+    
+};
